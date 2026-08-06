97625e46eec93846fd9a1bd72fddb186db999e4f
libaogang <237412637@qq.com>
2026-08-06 10:14:15 +0800

    工具响应截断加大

--- packages/service/core/workflow/dispatch/ai/tool/toolCall.ts
+++ packages/service/core/workflow/dispatch/ai/tool/toolCall.ts
@@ -325,7 +325,7 @@ export const runToolCall = async (props: DispatchToolModuleProps): Promise<Respo
               toolName: '',
               toolAvatar: '',
               params: '',
-              response: sliceStrStartEnd(response, 5000, 5000)
+              response: sliceStrStartEnd(response, 50000, 50000)
             }
           }
         });
@@ -367,7 +367,7 @@ export const runToolCall = async (props: DispatchToolModuleProps): Promise<Respo
               toolName: '',
               toolAvatar: '',
               params: '',
-              response: sliceStrStartEnd(stringToolResponse, 5000, 5000)
+              response: sliceStrStartEnd(stringToolResponse, 50000, 50000)
             }
           }
         });
